5418b73be0206b44ed8335ffda45816f72271dcb
Shano1993 <stefan.hanotiau@gmail.com>
2022-11-23 14:39:27 +0100

    typescript

--- assets/js/app.ts
+++ assets/js/app.ts
@@ -1,10 +1,1 @@
 import "../styles/style.scss";
-
-let monString: string = "Hello";
-let monNombre: number = 411515;
-
-console.log(monString);
-console.log(monNombre);
-
-let result: number = monNombre + parseInt(monString);
-console.log(result);
